6220f7ce08225d01ce2d09c69f893e906f019175
a-mochi <mochizuki@val.co.jp>
2016-01-07 11:48:01 +0900

    リポジトリ！

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fukushu::Application.config.secret_key_base = '79df57dce092180a66eee3deeab2e5f5c5beb7a9a21ac13078f811fc6e0233115930158d9ce27ed0ded881a55a5564ac98ae7730b2c76e0a3715f35a8eb57f49'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fukushu::Application.config.session_store :cookie_store, key: '_fukushu_session'
